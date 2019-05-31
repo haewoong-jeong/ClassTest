912f69e2c8c108d7f27a71c43f6a5c7600e5b0ca
haewoong-jeong <wjdgodnd315@naver.com>
2019-05-31 15:12:27 +0900

    초기 커밋.

---
+++ ClassTest/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTest
+{
+    class Animal
+    {
+        private string name;
+        private string sound;
+
+        public Animal()
+        {
+
+        }
+
+        public Animal(string n, string s)
+        {
+            name = n;
+            sound = s;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("{0} says {1}", name, sound);
+        }
+    }
+}
---
+++ ClassTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog = new Animal("dog", "bow");
+            dog.MakeSound();
+            Animal cow = new Animal("cow","soso");
+            cow.MakeSound();
+            Animal cat = new Animal("cat", "ㅋㅋ");
+            cat.MakeSound();
+
+        }
+    }
+}
